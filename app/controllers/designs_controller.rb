adf89111a3188be1bcb5bf1a6bc34aca32f26288
Acidicts <alexjhayes1@hotmail.com>
2026-06-16 21:38:34 +0000

    Creating "brand" css file and regulated form for new/edit design

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -3,7 +3,7 @@ class DesignsController < ApplicationController
   before_action :set_nav
   before_action :ensure_signed_in
   before_action :find_design, only: [ :show, :edit, :update ]
-  before_action :load_hackatime_projects, only: [ :edit ]
+  before_action :load_hackatime_projects, only: [ :new, :edit ]
 
   def index
     @designs = current_user.designs.order(updated_at: :desc)
