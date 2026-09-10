8cd1dbdb425be788e3ce3075a11401815e41b644
Acidicts <alexjhayes1@hotmail.com>
2026-09-10 22:35:17 +0000

    fix for rails ci

--- test/controllers/ship_request_controller_test.rb
+++ test/controllers/ship_request_controller_test.rb
@@ -1,13 +1,15 @@
 require "test_helper"
 
 class ShipRequestControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get ship_request_create_url
+  test "should get new" do
+    get new_ship_request_url(design_id: designs(:one).id)
     assert_response :success
   end
 
-  test "should get new" do
-    get ship_request_new_url
-    assert_response :success
+  test "should get create" do
+    assert_difference("ShipRequest.count", 1) do
+      post ship_requests_url, params: { ship_request: { design_id: designs(:one).id, title: "Test", body: "Body" } }
+    end
+    assert_redirected_to design_path(designs(:one))
   end
 end
